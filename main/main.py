fa7ec9a499b69c2cf3078f654842b1ca423bec31
Stiven <jsmoreno85@ucatolica.edu.co>
2026-03-02 17:47:20 -0500

    Unifica programal.py en main/main.py desde rama programador

---
+++ main/main.py
@@ -0,0 +1,9 @@
+
+numero = float(input("Ingrese un número: "))
+
+if numero > 0:
+    print(f"El número {numero} es POSITIVO")
+elif numero < 0:
+    print(f"El número {numero} es NEGATIVO")
+else:
+    print("El número es CERO")
